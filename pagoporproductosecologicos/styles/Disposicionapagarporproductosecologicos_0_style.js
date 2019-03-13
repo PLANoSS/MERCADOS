0e999f93377a407292aa8fe4a01c6a7485fae524
PLANoSS <freyesbueno@gmail.com>
2019-03-13 17:42:33 -0500

    Pago por productos ecológicos

--- pagoporproductosecologicos/styles/Disposicionapagarporproductosecologicos_0_style.js
+++ pagoporproductosecologicos/styles/Disposicionapagarporproductosecologicos_0_style.js
@@ -6,7 +6,7 @@ var style_Disposicionapagarporproductosecologicos_0 = function(feature, resoluti
         feature: feature,
         variables: {}
     };
-    var value = feature.get("q2wHide_No estaría");
+    var value = feature.get("No estaría dispuesto a pagar más (%)");
     var labelText = "";
     size = 0;
     var labelFont = "9.75px \'MS Shell Dlg 2\', sans-serif";
