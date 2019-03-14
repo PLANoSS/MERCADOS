f47511a20195f6171342fef8f3b2b1b3839cfeae
PLANoSS <freyesbueno@gmail.com>
2019-03-14 08:42:16 -0500

    Valor nutricional

--- valornutricional/styles/Valornutricionaldelosproductos_0_style.js
+++ valornutricional/styles/Valornutricionaldelosproductos_0_style.js
@@ -6,7 +6,7 @@ var style_Valornutricionaldelosproductos_0 = function(feature, resolution){
         feature: feature,
         variables: {}
     };
-    var value = feature.get("q2wHide_No muy inf");
+    var value = feature.get("No muy informado (%)");
     var labelText = "";
     size = 0;
     var labelFont = "9.75px \'MS Shell Dlg 2\', sans-serif";
